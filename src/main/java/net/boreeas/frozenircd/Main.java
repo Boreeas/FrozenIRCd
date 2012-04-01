19800635444f155c912020f585a17b2464dc644b
Boreeas <malte.schuetze@fgms.de>
2012-04-01 23:12:40 +0200

    Initial commit

---
+++ src/main/java/net/boreeas/frozenircd/Main.java
@@ -0,0 +1,16 @@
+package net.boreeas.frozenircd;
+
+/**
+ * This class controls the startup of the IRCd
+ * @author malte
+ */
+public class Main {
+
+    /**
+     * Program entry point
+     * @param args The command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
